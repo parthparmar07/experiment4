0bc67f219937ec8e2b80ab4e8e02fc13219d1129
Parth Parmar <parthparmar4105@gmail.com>
2026-02-10 10:26:20 +0530

    Add main.py with placeholder code for calculator app

---
+++ main.py
@@ -0,0 +1,11 @@
+# main.py - Simple Calculator Application
+
+# Placeholder for feature functions
+# These will be imported from separate modules after merging
+
+def main():
+    print("Simple Calculator")
+    # TODO: Call add(), sub(), mul() functions here
+
+if __name__ == "__main__":
+    main()
